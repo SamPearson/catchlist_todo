7a0e10edd6b0df561e3d09ee648589bbabb53400
Sam Pearson <sam.ed.pearson@gmail.com>
2025-04-13 18:40:12 -0500

    include path issues for db

--- src/api/gunicorn_config_api.py
+++ src/api/gunicorn_config_api.py
@@ -1,6 +1,8 @@
 from src.api.api import app
+from src.config.db_models import db
 from src.config.db_config import initialize_database
 
-
 def on_starting(server):
-    initialize_database(app)
+    with app.app_context():
+        db.create_all()  # This ensures tables exist
+        initialize_database(app)  # This handles any other initialization
