71c5577e3ddceca5d64df4b2ed757153388eba04
Sam Pearson <sam.ed.pearson@gmail.com>
2025-04-20 10:28:25 -0500

    CASCADE - delete user, delete orphaned todos

--- src/config/db_models.py
+++ src/config/db_models.py
@@ -4,18 +4,21 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
-    todos = db.relationship('Todo', backref='user', lazy=True)
+    todos = db.relationship('Todo', backref='user', lazy=True,
+                            cascade="all, delete-orphan")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
 
 class Todo(db.Model):
     __tablename__ = 'todo'
