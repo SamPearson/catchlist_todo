ae4a1a50f9e09a58258abc31c936feb9072fdca5
sampearson <sam.ed.pearson@gmail.com>
2025-12-12 06:19:52 -0800

    tag association class

--- src/database/tags/models.py
+++ src/database/tags/models.py
@@ -1,19 +1,36 @@
-
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from src.database.base.models import UserOwnedModel
-from sqlalchemy import Column, String
+from src.database.db import db
+
+
+class TagAssociation(db.Model):
+    """Association model for connecting tags to any entity"""
+    __tablename__ = 'tag_association'
+
+    id = Column(Integer, primary_key=True)
+    tag_id = Column(Integer, ForeignKey('tag.id', ondelete='CASCADE'), nullable=False)
+    entity_id = Column(Integer, nullable=False)
+    entity_type = Column(String(50), nullable=False)
+
+    # Index for faster lookups - combine with extend_existing
+    __table_args__ = (
+        db.Index('idx_tag_entity', 'entity_type', 'entity_id'),
+    )
 
 
 class Tag(UserOwnedModel):
-    """
-    Tag model representing user-defined tags for categorizing items.
-    Designed to be attachable to tasks and other future entities.
-    """
+    """Tag model representing user-defined tags"""
+    __tablename__ = 'tag'
 
     name = Column(String(50), nullable=False)
     color = Column(String(10), default='#6c757d')
 
+    associations = relationship("TagAssociation",
+                              backref='tag',
+                              cascade='all, delete-orphan')
+
     def as_dict(self):
-        """Convert tag to dictionary representation"""
         data = super().as_dict()
         data.update({
             'name': self.name,
