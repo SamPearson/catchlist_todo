4d8342dfb1cb1ebf38018fc96f2def10184c7e14
sampearson <sam.ed.pearson@gmail.com>
2025-12-23 07:58:33 -0800

    attach and detatch tag routes

--- src/api/routes/tags/__init__.py
+++ src/api/routes/tags/__init__.py
@@ -10,3 +10,5 @@ tags_bp.add_url_rule('/api/tags', view_func=tags.create_tag, endpoint='create_ta
 tags_bp.add_url_rule('/api/tags/<int:tag_id>', view_func=tags.get_tag, endpoint='get_tag', methods=['GET'])
 tags_bp.add_url_rule('/api/tags/<int:tag_id>', view_func=tags.update_tag, endpoint='update_tag', methods=['PUT'])
 tags_bp.add_url_rule('/api/tags/<int:tag_id>', view_func=tags.delete_tag, endpoint='delete_tag', methods=['DELETE'])
+tags_bp.add_url_rule("/api/tags/attach", view_func=tags.attach_tag, methods=["POST"])
+tags_bp.add_url_rule("/api/tags/detach", view_func=tags.detach_tag, methods=["POST"])
