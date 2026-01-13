4e46d203f18ff4f2f1e574f4cd428013e382af8a
Sam Pearson <sam.ed.pearson@gmail.com>
2026-01-12 22:03:07 -0600

    removed unnecessary endpoints

--- src/api/routes/projects/__init__.py
+++ src/api/routes/projects/__init__.py
@@ -13,5 +13,3 @@ projects_bp.add_url_rule("/api/projects/<int:project_id>", view_func=projects.de
 # Project Task Routes
 projects_bp.add_url_rule("/api/projects/<int:project_id>/tasks", view_func=projects.get_project_tasks, methods=["GET"])
 projects_bp.add_url_rule("/api/projects/<int:project_id>/tasks", view_func=projects.create_project_task, methods=["POST"])
-projects_bp.add_url_rule("/api/projects/<int:project_id>/tasks/<int:task_id>", view_func=projects.update_project_task, methods=["PUT"])
-projects_bp.add_url_rule("/api/projects/<int:project_id>/tasks/<int:task_id>", view_func=projects.delete_project_task, methods=["DELETE"])
