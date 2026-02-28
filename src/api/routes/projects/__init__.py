30037041cd8f5aea16114d1ea5293b095039ac74
Sam Pearson <sam.ed.pearson@gmail.com>
2026-02-28 16:53:21 -0600

    completion and activation endpoint registration

--- src/api/routes/projects/__init__.py
+++ src/api/routes/projects/__init__.py
@@ -10,6 +10,12 @@ projects_bp.add_url_rule("/api/projects/<int:project_id>", view_func=projects.ge
 projects_bp.add_url_rule("/api/projects/<int:project_id>", view_func=projects.update_project, methods=["PUT"])
 projects_bp.add_url_rule("/api/projects/<int:project_id>", view_func=projects.delete_project, methods=["DELETE"])
 
+# Project State Management Routes
+projects_bp.add_url_rule("/api/projects/<int:project_id>/complete", view_func=projects.complete_project, methods=["PATCH"])
+projects_bp.add_url_rule("/api/projects/<int:project_id>/uncomplete", view_func=projects.uncomplete_project, methods=["PATCH"])
+projects_bp.add_url_rule("/api/projects/<int:project_id>/activate", view_func=projects.activate_project, methods=["PATCH"])
+projects_bp.add_url_rule("/api/projects/<int:project_id>/deactivate", view_func=projects.deactivate_project, methods=["PATCH"])
+
 # Project Task Routes
 projects_bp.add_url_rule("/api/projects/<int:project_id>/tasks", view_func=projects.get_project_tasks, methods=["GET"])
 projects_bp.add_url_rule("/api/projects/<int:project_id>/tasks", view_func=projects.create_project_task, methods=["POST"])
