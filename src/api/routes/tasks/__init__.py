e0e9341c5d74b6eac13cc9f58da58739cfee7311
Sam Pearson <sam.ed.pearson@gmail.com>
2026-02-28 16:54:58 -0600

    adding activate/deactivate endpoints, using patch method where appropriate

--- src/api/routes/tasks/__init__.py
+++ src/api/routes/tasks/__init__.py
@@ -10,8 +10,15 @@ tasks_bp.add_url_rule('/api/tasks', view_func=tasks.create_task, endpoint='creat
 tasks_bp.add_url_rule('/api/tasks/<int:task_id>', view_func=tasks.get_task, endpoint='get_task', methods=['GET'])
 tasks_bp.add_url_rule('/api/tasks/<int:task_id>', view_func=tasks.update_task, endpoint='update_task', methods=['PUT'])
 tasks_bp.add_url_rule('/api/tasks/<int:task_id>', view_func=tasks.delete_task, endpoint='delete_task', methods=['DELETE'])
-tasks_bp.add_url_rule('/api/tasks/<int:task_id>/complete', view_func=tasks.complete_task, endpoint='complete_task', methods=['POST'])
-tasks_bp.add_url_rule('/api/tasks/<int:task_id>/uncomplete', view_func=tasks.uncomplete_task, endpoint='uncomplete_task', methods=['POST'])
-tasks_bp.add_url_rule('/api/tasks/<int:task_id>/attach/<int:project_id>', view_func=tasks.attach_to_project, endpoint='attach_to_project', methods=['POST'])
-tasks_bp.add_url_rule('/api/tasks/<int:task_id>/detach', view_func=tasks.detach_from_project, endpoint='detach_from_project', methods=['POST'])
+
+# Task State Management Routes
+tasks_bp.add_url_rule('/api/tasks/<int:task_id>/complete', view_func=tasks.complete_task, endpoint='complete_task', methods=['PATCH'])
+tasks_bp.add_url_rule('/api/tasks/<int:task_id>/uncomplete', view_func=tasks.uncomplete_task, endpoint='uncomplete_task', methods=['PATCH'])
+tasks_bp.add_url_rule('/api/tasks/<int:task_id>/activate', view_func=tasks.activate_task, endpoint='activate_task', methods=['PATCH'])
+tasks_bp.add_url_rule('/api/tasks/<int:task_id>/deactivate', view_func=tasks.deactivate_task, endpoint='deactivate_task', methods=['PATCH'])
+tasks_bp.add_url_rule('/api/tasks/<int:task_id>/status', view_func=tasks.change_task_status, endpoint='change_task_status', methods=['PATCH'])
+
+# Task Project Association Routes
+tasks_bp.add_url_rule('/api/tasks/<int:task_id>/attach/<int:project_id>', view_func=tasks.attach_to_project, endpoint='attach_to_project', methods=['PATCH'])
+tasks_bp.add_url_rule('/api/tasks/<int:task_id>/detach', view_func=tasks.detach_from_project, endpoint='detach_from_project', methods=['PATCH'])
 
