689b446deb43c017c58986389e105b313c47acb8
Sam Pearson <sam.ed.pearson@gmail.com>
2026-03-26 18:41:19 -0500

    Changing calendar update endpoint from "PUT" to "PATCH"

--- src/api/routes/calendars/__init__.py
+++ src/api/routes/calendars/__init__.py
@@ -8,7 +8,7 @@ calendars_bp.add_url_rule('/api/calendars/<int:calendar_id>', 'get_calendar', ca
 calendars_bp.add_url_rule("/api/calendars", view_func=calendars.create_calendar, methods=["POST"])
 calendars_bp.add_url_rule("/api/calendars/discover", view_func=calendars.discover_calendars, methods=["POST"])
 calendars_bp.add_url_rule("/api/calendars/sync", view_func=calendars.sync_calendar, methods=["POST"])
-calendars_bp.add_url_rule("/api/calendars/<int:calendar_id>", view_func=calendars.update_calendar, methods=["PUT"])
+calendars_bp.add_url_rule("/api/calendars/<int:calendar_id>", view_func=calendars.update_calendar, methods=["PATCH"])
 calendars_bp.add_url_rule("/api/calendars/<int:calendar_id>/activate", view_func=calendars.activate_calendar, methods=["PATCH"])
 calendars_bp.add_url_rule("/api/calendars/<int:calendar_id>/deactivate", view_func=calendars.deactivate_calendar, methods=["PATCH"])
 calendars_bp.add_url_rule("/api/calendars/<int:calendar_id>", view_func=calendars.delete_calendar, methods=["DELETE"])
