b0a453678a3ae1ce1461681e63002ec58cf8aac7
Sam Pearson <sam.ed.pearson@gmail.com>
2026-04-06 12:40:16 -0500

    Deleting unused file

--- src/database/initialize_db.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config.db_config import Config, initialize_database
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-if __name__ == '__main__':
-    initialize_database(app) 
