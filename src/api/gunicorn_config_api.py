5426ae9e8cfcc148b8e977216bdecf8dc3b86f64
Sam Pearson <sam.ed.pearson@gmail.com>
2026-04-21 16:11:12 -0500

    removing gunicorn config

--- src/api/gunicorn_config_api.py
+++
@@ -1,8 +0,0 @@
-from src.api.api import app
-from src.database.db import db
-from src.database.config_db import initialize_database
-
-def on_starting(server):
-    with app.app_context():
-        db.create_all()  # This ensures tables exist
-        initialize_database(app)  # This handles any other initialization
