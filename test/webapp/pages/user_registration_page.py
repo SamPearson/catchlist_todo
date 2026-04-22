c0e2f8d9e7d90259304c57f87e6f4b76f2887da0
Sam Pearson <sam.ed.pearson@gmail.com>
2026-04-22 11:12:40 -0500

    new user registration page object

--- test/webapp/pages/user_registration_page.py
+++ test/webapp/pages/user_registration_page.py
@@ -1,21 +1,28 @@
 from pages.base_app_page import BaseAppPage
-from pages.base_page import testid_locator
+from pages.base_page import locator_from_testid
 
 
 class RegistrationPage(BaseAppPage):
-    username_input_locator = testid_locator("register-username")
-    password_input_locator = testid_locator("register-password")
-    password_confirmation_locator = testid_locator("register-confirm-password")
-    register_button_locator = testid_locator("register-submit")
+    username_input_locator = locator_from_testid("register-username-input")
+    password_input_locator = locator_from_testid("register-password-input")
+    submit_button_locator = locator_from_testid("register-submit-button")
+    success_message_locator = locator_from_testid("register-success-message")
+    error_message_locator = locator_from_testid("register-error-message")
+    login_link_locator = locator_from_testid("register-login-link")
 
     def __init__(self, driver):
         super().__init__(driver)
-        self._visit("/register")
+        self._visit("/auth/register")
 
     def register_new_user(self, username, password):
+        """Register a new user account"""
         self._type(self.username_input_locator, username)
         self._type(self.password_input_locator, password)
-        self._type(self.password_confirmation_locator, password)
-        self._click(self.register_button_locator)
-        # Wait for redirect to login page
-        self.wait_for_url('contains', '/login')
+        self._click(self.submit_button_locator)
+        
+        # Wait for success message and redirect
+        self._wait_for_url('contains', '/auth/login', timeout=10)
+
+    def has_error_message(self):
+        """Check if an error message is displayed"""
+        return self._is_displayed(self.error_message_locator, timeout=2)
