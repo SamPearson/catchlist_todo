9887e846eb7fd06e6d34fc97da42f899283afb3f
Sam Pearson <sam.ed.pearson@gmail.com>
2026-04-22 11:12:45 -0500

    new login page object

--- test/webapp/pages/login_page.py
+++ test/webapp/pages/login_page.py
@@ -1,23 +1,31 @@
 from pages.base_app_page import BaseAppPage
-from pages.base_page import testid_locator
+from pages.base_page import locator_from_testid
 
 
 class LoginPage(BaseAppPage):
-    username_field_locator = testid_locator("username-input")
-    password_field_locator = testid_locator("password-input")
-    login_button_locator = testid_locator("login-button")
+    username_input_locator = locator_from_testid("login-username-input")
+    password_input_locator = locator_from_testid("login-password-input")
+    submit_button_locator = locator_from_testid("login-submit-button")
+    error_message_locator = locator_from_testid("login-error-message")
+    register_link_locator = locator_from_testid("login-register-link")
 
     def __init__(self, driver):
         super().__init__(driver)
-        self._visit("/login")
+        self._visit("/auth/login")
 
     def login(self, username, password):
-        self._click(self.login_link_locator)
-        self._type(self.username_field_locator, username)
-        self._type(self.password_field_locator, password)
-        self._click(self.login_button_locator)
+        """Log in with provided credentials"""
+        self._type(self.username_input_locator, username)
+        self._type(self.password_input_locator, password)
+        self._click(self.submit_button_locator)
 
-        # some assertions to make sure we are past the login page
-        self.wait_for_url('contains', '/desk')
-        assert self._is_active(self.logout_link_locator, 5), "Could not find the logout button after logging in"
+        # Wait for redirect to home page after successful login
+        self._wait_for_url('is', self.driver.base_url + '/', timeout=10)
+        
+        # Verify we're logged in by checking for logout button in navbar
+        assert self._is_active(self.navbar_logout_button_locator, 5), "Could not find the logout button after logging in"
+
+    def has_error_message(self):
+        """Check if an error message is displayed"""
+        return self._is_displayed(self.error_message_locator, timeout=2)
 
