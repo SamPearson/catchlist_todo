b9e9aa1babd870feb74729894c62adc326fc2885
Sam Pearson <sam.ed.pearson@gmail.com>
2026-09-18 07:57:02 -0500

    bringing the task components into the new style

--- src/webapp/static/js/components/tasks/task_card.js
+++ src/webapp/static/js/components/tasks/task_card.js
@@ -171,13 +171,13 @@ function taskCard(initialTask) {
 
         getStatusTagClass() {
             const statusClasses = {
-                'open': 'is-success is-light',
-                'waiting': 'is-warning is-light',
-                'deferred': 'is-info is-light',
-                'declined': 'is-danger is-light',
-                'stale': 'is-light'
+                'open': 'tag-success',
+                'waiting': 'tag-warning',
+                'deferred': 'tag-info',
+                'declined': 'tag-danger',
+                'stale': 'tag-grey'
             };
-            return statusClasses[this.task.status] || 'is-light';
+            return statusClasses[this.task.status] || 'tag-grey';
         },
 
         formatDate(dateString) {
